394c38a98251c0dfd54bfa7d4c53d87d6016b164
root <root@terraform-hw4.ru-central1.internal>
2024-09-04 21:27:36 +0000

    Changes for terraform-hotfix

--- hw/providers.tf
+++ hw/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.75.0"
+      version = "0.128.0"
     }
     template = {
       source  = "hashicorp/template"
